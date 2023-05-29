2961d6ef04a2f9a4499da8b076abec58e0b65c77
Gaurav verma <gaurav.verma@almabetter.com>
2023-05-29 12:23:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use((req,res)=>{
+  res.send('hello Almabetter')
+})
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
