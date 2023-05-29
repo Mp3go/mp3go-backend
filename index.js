9deb81ac12a2932dc6914284ef706486036e975f
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-29 10:55:48 +0530

    Added .env file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Now listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
 });
