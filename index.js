32a691c8d49e2245930c087f8e960604e5fa1046
NisthaAB <nistha.ghorela@almabetter.com>
2023-05-31 19:09:42 +0530

    Added header

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 // const path = require("path");
 // const authRoutes = require("./routes/auth");
-const albumRoutes = require("./routes/albums");
+const albumRoutes = require('./routes/albums');
 // const userRoutes = require("./routes/user");
-const axios = require("axios");
-const Music = require("./models/music");
-const Filter = require("./models/filter");
-require("dotenv").config();
+const axios = require('axios');
+const Music = require('./models/music');
+const Filter = require('./models/filter');
+
+require('dotenv').config();
 const app = express();
 const Port = process.env.port || 3000;
-app.use("/albums", albumRoutes);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
+app.use('/albums', albumRoutes);
 // app.use(authRoutes);
 // app.use("/user", userRoutes);
 
@@ -22,11 +38,11 @@ mongoose
   )
   .then(() => {
     app.listen(Port, () => {
-      console.log("Listening on Port");
+      console.log('Listening on Port');
     });
 
-    console.log("Connected to Database");
+    console.log('Connected to Database');
   })
   .catch((err) => {
-    console.log("Error in Database", err);
+    console.log('Error in Database', err);
   });
